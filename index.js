9005f149642b64a6ba5d0910faaa8ec83d003c75
YangHyunYong <9935ok9938@gmail.com>
2021-01-04 19:28:24 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const port = 5000
+const bodyParser = require('body-parser')
+const { User } = require("./models/User")
+//application/x-www-form-urlencoded 형태를 분석, 가져오는데 사용
+app.use(bodyParser.urlencoded({extended: true}))
+
+//application/json 형태를 분석, 가져오는데 사용
+app.use(bodyParser.json())
+
+const mongoose = require('mongoose')
+mongoose.connect('',{
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(()=>console.log('MongoDB Connected...'))
+.catch(err => console.log(err))
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.post('/register', (req,res)=>{
+    //회원가입 정보를 client에서 가져오고 DB에 저장
+    const user = new User(req.body)
+    user.save((err, userinfo) =>{
+        if(err) return res.json({success:false, err})   //실패시
+        return res.status(200).json({success: true})    //성공시
+    }) //몽고db 메서드
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
